692b14c02f4c9373fb12b705673a0a96b0ea3b93
siricarsen-cmd <siri.carsen@gmail.com>
2026-08-22 08:09:54 -0700

    SEO: add constructive changes guide

---
+++ app/blog/constructive-changes-federal-subcontracts/page.tsx
@@ -0,0 +1,4 @@
+import { Batch7ArticlePage } from "../components/Batch7ArticlePage";
+import { createPublicMetadata } from "@/lib/seo";
+export const metadata = createPublicMetadata("/blog/constructive-changes-federal-subcontracts");
+export default function Page(){return <Batch7ArticlePage slug="constructive-changes-federal-subcontracts"/>;}
