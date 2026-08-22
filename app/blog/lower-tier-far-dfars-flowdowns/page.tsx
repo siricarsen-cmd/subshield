d9783d492e17aa41ea3960ae65de800b430ec122
siricarsen-cmd <siri.carsen@gmail.com>
2026-08-22 08:06:51 -0700

    SEO: add lower-tier flowdown guide

---
+++ app/blog/lower-tier-far-dfars-flowdowns/page.tsx
@@ -0,0 +1,4 @@
+import { Batch6ArticlePage } from "../components/Batch6ArticlePage";
+import { createPublicMetadata } from "@/lib/seo";
+export const metadata = createPublicMetadata("/blog/lower-tier-far-dfars-flowdowns");
+export default function Page(){return <Batch6ArticlePage slug="lower-tier-far-dfars-flowdowns"/>;}
