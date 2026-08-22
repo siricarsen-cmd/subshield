ff4b3b84fb4c5a389355dd78b2ca48db11f98a7f
siricarsen-cmd <siri.carsen@gmail.com>
2026-08-21 19:36:08 -0700

    SEO: add differing site conditions page

---
+++ app/blog/differing-site-conditions-federal-subcontract/page.tsx
@@ -0,0 +1,1 @@
+import { Batch5ArticlePage } from "../components/Batch5ArticlePage"; import { createPublicMetadata } from "@/lib/seo"; export const metadata=createPublicMetadata("/blog/differing-site-conditions-federal-subcontract"); export default function Page(){return <Batch5ArticlePage slug="differing-site-conditions-federal-subcontract"/>;}
