ad5423e16dddfd8e39ef28d86f5a8831f5d70a56
siricarsen-cmd <siri.carsen@gmail.com>
2026-08-22 08:06:32 -0700

    SEO: add DFARS 7012 subcontractor guide

---
+++ app/blog/dfars-252-204-7012-subcontractors/page.tsx
@@ -0,0 +1,4 @@
+import { Batch6ArticlePage } from "../components/Batch6ArticlePage";
+import { createPublicMetadata } from "@/lib/seo";
+export const metadata = createPublicMetadata("/blog/dfars-252-204-7012-subcontractors");
+export default function Page(){return <Batch6ArticlePage slug="dfars-252-204-7012-subcontractors"/>;}
