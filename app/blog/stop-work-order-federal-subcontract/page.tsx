615518111af4d9c7be81965e72db1c2bc06794dd
siricarsen-cmd <siri.carsen@gmail.com>
2026-08-21 19:36:03 -0700

    SEO: add stop work page

---
+++ app/blog/stop-work-order-federal-subcontract/page.tsx
@@ -0,0 +1,1 @@
+import { Batch5ArticlePage } from "../components/Batch5ArticlePage"; import { createPublicMetadata } from "@/lib/seo"; export const metadata=createPublicMetadata("/blog/stop-work-order-federal-subcontract"); export default function Page(){return <Batch5ArticlePage slug="stop-work-order-federal-subcontract"/>;}
