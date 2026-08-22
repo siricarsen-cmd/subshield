be14ccf8ea89d0ed26517f1603a8e76df2f36736
siricarsen-cmd <siri.carsen@gmail.com>
2026-08-22 08:06:37 -0700

    SEO: add DoD cyber incident reporting guide

---
+++ app/blog/dod-cyber-incident-reporting-72-hours/page.tsx
@@ -0,0 +1,4 @@
+import { Batch6ArticlePage } from "../components/Batch6ArticlePage";
+import { createPublicMetadata } from "@/lib/seo";
+export const metadata = createPublicMetadata("/blog/dod-cyber-incident-reporting-72-hours");
+export default function Page(){return <Batch6ArticlePage slug="dod-cyber-incident-reporting-72-hours"/>;}
