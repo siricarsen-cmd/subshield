54cc838f4a8935d317b7f1683c3046a7dcd7c1c7
siricarsen-cmd <siri.carsen@gmail.com>
2026-08-22 08:06:45 -0700

    SEO: add CMMC supplier questionnaire guide

---
+++ app/blog/cmmc-supplier-questionnaire-subcontractors/page.tsx
@@ -0,0 +1,4 @@
+import { Batch6ArticlePage } from "../components/Batch6ArticlePage";
+import { createPublicMetadata } from "@/lib/seo";
+export const metadata = createPublicMetadata("/blog/cmmc-supplier-questionnaire-subcontractors");
+export default function Page(){return <Batch6ArticlePage slug="cmmc-supplier-questionnaire-subcontractors"/>;}
