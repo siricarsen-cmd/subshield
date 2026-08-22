b583169cfd495ca71e0efb93f71c231cfd6fef70
siricarsen-cmd <siri.carsen@gmail.com>
2026-08-22 08:10:03 -0700

    SEO: add dispute venue arbitration guide

---
+++ app/blog/dispute-venue-arbitration-federal-subcontracts/page.tsx
@@ -0,0 +1,4 @@
+import { Batch7ArticlePage } from "../components/Batch7ArticlePage";
+import { createPublicMetadata } from "@/lib/seo";
+export const metadata = createPublicMetadata("/blog/dispute-venue-arbitration-federal-subcontracts");
+export default function Page(){return <Batch7ArticlePage slug="dispute-venue-arbitration-federal-subcontracts"/>;}
