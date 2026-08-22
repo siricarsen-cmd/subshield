5fde3cfe8953151297f57501bcd86f3596db391d
siricarsen-cmd <siri.carsen@gmail.com>
2026-08-21 19:36:39 -0700

    SEO: register batch 5 articles

--- app/blog/postRegistry.ts
+++ app/blog/postRegistry.ts
@@ -1,4 +1,5 @@
 import { posts as existingPosts, type Post } from "./articleData";
 import { batch4Posts } from "./batch4Data";
+import { batch5Posts } from "./batch5Data";
 
-export const posts: Post[] = [...batch4Posts, ...existingPosts];
+export const posts: Post[] = [...batch5Posts, ...batch4Posts, ...existingPosts];
