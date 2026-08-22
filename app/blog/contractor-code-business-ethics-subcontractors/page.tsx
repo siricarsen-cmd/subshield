101f376361572eff7698b03ba7cb1de4aff25fc0
siricarsen-cmd <siri.carsen@gmail.com>
2026-08-21 19:36:19 -0700

    SEO: add ethics flowdown page

---
+++ app/blog/contractor-code-business-ethics-subcontractors/page.tsx
@@ -0,0 +1,1 @@
+import { Batch5ArticlePage } from "../components/Batch5ArticlePage"; import { createPublicMetadata } from "@/lib/seo"; export const metadata=createPublicMetadata("/blog/contractor-code-business-ethics-subcontractors"); export default function Page(){return <Batch5ArticlePage slug="contractor-code-business-ethics-subcontractors"/>;}
