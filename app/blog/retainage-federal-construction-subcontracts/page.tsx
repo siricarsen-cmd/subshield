aac0dbbc46b3cf60fa19545c54afb0866140e483
siricarsen-cmd <siri.carsen@gmail.com>
2026-08-22 08:07:12 -0700

    SEO: add federal construction retainage guide

---
+++ app/blog/retainage-federal-construction-subcontracts/page.tsx
@@ -0,0 +1,4 @@
+import { Batch6ArticlePage } from "../components/Batch6ArticlePage";
+import { createPublicMetadata } from "@/lib/seo";
+export const metadata = createPublicMetadata("/blog/retainage-federal-construction-subcontracts");
+export default function Page(){return <Batch6ArticlePage slug="retainage-federal-construction-subcontracts"/>;}
