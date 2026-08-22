e902790d6817329b1499a4981357c6ca0a2f4601
siricarsen-cmd <siri.carsen@gmail.com>
2026-08-22 08:10:49 -0700

    SEO: add missing SOW exhibits guide

---
+++ app/blog/missing-statement-of-work-exhibits-subcontract/page.tsx
@@ -0,0 +1,4 @@
+import { Batch7ArticlePage } from "../components/Batch7ArticlePage";
+import { createPublicMetadata } from "@/lib/seo";
+export const metadata = createPublicMetadata("/blog/missing-statement-of-work-exhibits-subcontract");
+export default function Page(){return <Batch7ArticlePage slug="missing-statement-of-work-exhibits-subcontract"/>;}
